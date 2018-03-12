baafccb71bb04939281c8e3db907bf9e2b81afd5
Timo Maier <tmaier96@gmail.com>
2018-03-12 18:01:30 +0100

    Reimplement ArchitectureVersion, now inheriting from KAMP4aPS

--- edu.kit.ipd.sdq.kamp4aps4req.hardware/src/edu/kit/ipd/sdq/kamp4aps4req/hardware/APSReqHardwareArchitectureVersion.java
+++ edu.kit.ipd.sdq.kamp4aps4req.hardware/src/edu/kit/ipd/sdq/kamp4aps4req/hardware/APSReqHardwareArchitectureVersion.java
@@ -1,48 +1,82 @@
 package edu.kit.ipd.sdq.kamp4aps4req.hardware;
 
 import options.OptionRepository;
+import options.OptionsFactory;
 import decisions.DecisionRepository;
-import edu.kit.ipd.sdq.kamp4aps4req.core.AbstractAPSReqArchitectureVersion;
+import decisions.DecisionsFactory;
 import edu.kit.ipd.sdq.kamp4aps.core.APSArchitectureVersion;
+import edu.kit.ipd.sdq.kamp4aps.model.DeploymentContext.DeploymentContextRepository;
+import edu.kit.ipd.sdq.kamp4aps.model.aPS.Plant;
+import edu.kit.ipd.sdq.kamp4aps.model.fieldofactivityannotations.FieldOfActivityAnnotationRepository;
 import edu.kit.ipd.sdq.kamp4aps4req.model.modificationmarks_hardware.APSReqHardwareModificationRepository;
+import edu.kit.ipd.sdq.kamp4iec.model.IECFieldOfActivityAnnotations.IECFieldOfActivityAnnotationsRepository;
+import edu.kit.ipd.sdq.kamp4iec.model.IECModel.Configuration;
+import edu.kit.ipd.sdq.kamp4iec.model.IECModificationmarks.IECModificationRepository;
+import edu.kit.ipd.sdq.kamp4iec.model.IECRepository.Repository;
 import requirements.ReqRepository;
+import requirements.RequirementsFactory;
 
 /**
  * Sub class of APSReqArchitectureVersion, representing the software side
  * @author Timo Maier
  *
  */
-public class APSReqHardwareArchitectureVersion extends AbstractAPSReqArchitectureVersion<APSReqHardwareModificationRepository> {
+public class APSReqHardwareArchitectureVersion extends APSArchitectureVersion {
 
-	/**
-	 * Represents architecture version of the hardware
-	 */
-	private APSArchitectureVersion apsArchitectureVersion;
+	private ReqRepository requirementsRepository;
+	private DecisionRepository decisionRepository;
+	private OptionRepository optionRepository;
 	
-	/**
-	 * Constructor
-	 * @param name Name of Architecture Version
-	 * @param requirementsRepository Requirements Repository
-	 * @param decisionRepository Decision Repository
-	 * @param optionRepository Option Repository
-	 * @param modificationMarksRepository Modification marks Repository
-	 * @param apsArchitectureVersion Architecture Version of the hardware in the system
-	 */
-	public APSReqHardwareArchitectureVersion(String name, ReqRepository requirementsRepository, DecisionRepository decisionRepository,
+	public APSReqHardwareArchitectureVersion(String name, FieldOfActivityAnnotationRepository fieldOfActivityRepository,
+			Plant aPSPlant, DeploymentContextRepository deploymentContextRepository, Repository iecRepository, 
+			Configuration configuration, IECFieldOfActivityAnnotationsRepository iecFieldOfActivityRepository,
+			IECModificationRepository iecModificationMarkRepository,
+			ReqRepository requirementsRepository, DecisionRepository decisionRepository,
 			OptionRepository optionRepository, 
-			APSReqHardwareModificationRepository modificationMarksRepository,
-			APSArchitectureVersion apsArchitectureVersion) {
-		
-		super(name, requirementsRepository, decisionRepository, optionRepository, modificationMarksRepository);
-		this.setApsArchitectureVersion(apsArchitectureVersion);
+			APSReqHardwareModificationRepository modificationMarksRepository) {
+		super(name, fieldOfActivityRepository, modificationMarksRepository, aPSPlant, deploymentContextRepository, iecRepository,
+				configuration, iecFieldOfActivityRepository, iecModificationMarkRepository);
+		if (requirementsRepository == null) {
+			requirementsRepository = RequirementsFactory.eINSTANCE.createReqRepository();
+		}
+		this.setRequirementsRepository(requirementsRepository);
+		if (decisionRepository == null) {
+			decisionRepository = DecisionsFactory.eINSTANCE.createDecisionRepository();
+		}
+		this.setDecisionRepository(decisionRepository);
+		if (optionRepository == null) {
+			optionRepository = OptionsFactory.eINSTANCE.createOptionRepository();
+		}
+		this.setOptionRepository(optionRepository);
+	}
+	
+	public ReqRepository getRequirementsRepository() {
+		return requirementsRepository;
 	}
 
-	public APSArchitectureVersion getApsArchitectureVersion() {
-		return apsArchitectureVersion;
+
+	public void setRequirementsRepository(ReqRepository requirementsRepository) {
+		this.requirementsRepository = requirementsRepository;
 	}
 
-	public void setApsArchitectureVersion(APSArchitectureVersion apsArchitectureVersion) {
-		this.apsArchitectureVersion = apsArchitectureVersion;
+
+	public DecisionRepository getDecisionRepository() {
+		return decisionRepository;
+	}
+
+
+	public void setDecisionRepository(DecisionRepository decisionRepository) {
+		this.decisionRepository = decisionRepository;
+	}
+
+
+	public OptionRepository getOptionRepository() {
+		return optionRepository;
+	}
+
+
+	public void setOptionRepository(OptionRepository optionRepository) {
+		this.optionRepository = optionRepository;
 	}
 	
 }
